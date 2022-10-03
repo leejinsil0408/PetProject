1a2f552b10867ea5190256d7b787763896d3b159
leejinsil0408 <hby04011@naver.com>
2022-10-04 08:53:17 +0900

    20221004

--- src/main/java/com/example/petproject01/PetProject01Application.java
+++ src/main/java/com/example/petproject01/PetProject01Application.java
@@ -3,6 +3,8 @@ package com.example.petproject01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.IntStream;
+
 @SpringBootApplication
 public class PetProject01Application {
 
